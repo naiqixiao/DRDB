52a38af0813a6cc1eb1c5d3f93ba41c4830e2b70
naiqixiao <naiqi.xiao@kangleelab.com>
2020-03-29 08:28:28 -0400

    delete server

--- server/api/routes/experimenter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const checkAuth = require("../middleware/check-auth");
-
-const ExperimenterController = require("../controllers/experimenter");
-
-router.post("/add", checkAuth, ExperimenterController.create);
-
-module.exports = router;
