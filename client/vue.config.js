cae739bca3b38a5fa0500e3bc1ac9ad4273aa120
naiqixiao <naiqi.xiao@kangleelab.com>
2020-07-04 19:06:31 -0400

    config proxy bug fix

--- client/vue.config.js
+++ client/vue.config.js
@@ -7,9 +7,9 @@ module.exports = {
     proxy: {
       "/api": {
         target: "http://localhost:3000/",
-      },
-      pathRewrite: {
-        "/api": "",
+        pathRewrite: {
+          "^/api": "",
+        },
       },
     },
   },
