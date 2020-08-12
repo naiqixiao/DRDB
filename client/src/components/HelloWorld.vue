35b9a1b6b9514a58cfa1499f312872ba9976d811
naiqixiao <naiqi.xiao@kangleelab.com>
2020-08-12 18:41:26 -0400

    version update

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
     <v-row class="text-center" align="center" style="height: 600px;">
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Developmental Research Management System V1.0.20200809
+          Developmental Research Management System V1.0.20200812
         </h1>
       </v-col>
     </v-row>
