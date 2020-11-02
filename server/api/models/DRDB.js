15a629abe9b97e48385c68a2c1db4a1c61d201a6
naiqixiao <naiqi.xiao@kangleelab.com>
2020-11-02 09:22:53 -0500

    Database config

--- server/api/models/DRDB.js
+++ server/api/models/DRDB.js
@@ -1,39 +1,8 @@
-const Sequelize = require("sequelize");
-const { Op } = require("sequelize");
+// const { Op } = require("sequelize");
 
-const sequelize = new Sequelize("DRDB", "admin", "password", {
-  host: "localhost",
-  dialect: "mysql",
-  logging: false,
-  define: {
-    // timestamps: false,
-    freezeTableName: true,
-  },
-  timeZone: "America/Toronto",
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+const config = require("../../config/general");
 
-// const sequelize = new Sequelize("pnb_drdb", "pnb_drdb", "kohta37ahtae", {
-//   host: "mysql",
-//   dialect: "mysql",
-//   logging: false,
-//   define: {
-//     // timestamps: false,
-//     freezeTableName: true,
-//   },
-//   timeZone: "America/Toronto",
-//   pool: {
-//     max: 10,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000,
-//   },
-// });
+const sequelize = config.sequelize;
 
 sequelize
   .authenticate()
--- server/config/general_EXAMPLE.js
+++ server/config/general_EXAMPLE.js
@@ -1,1 +1,18 @@
 exports.URL = 'URL of the system';
+
+exports.sequelize = new Sequelize("Database_Name", "User", "Password", {
+  host: "mysql",
+  dialect: "mysql",
+  logging: false,
+  define: {
+    // timestamps: false,
+    freezeTableName: true,
+  },
+  timeZone: "America/Toronto",
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+});
