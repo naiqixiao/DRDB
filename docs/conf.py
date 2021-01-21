3e795be143c2fbfabfd482172ad09b37292b4015
naiqixiao <naiqi.xiao@kangleelab.com>
2021-01-20 20:15:12 -0500

    ver update

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Gabriel (Naiqi) Xiao'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.0.20200911'
+release = '1.0.20210119'
 
 
 # -- General configuration ---------------------------------------------------
