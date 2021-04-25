8e38455fcca95eb2f2281e56719056281bf8d0c5
naiqixiao <naiqi.xiao@kangleelab.com>
2021-04-25 11:38:46 -0400

    ver update

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -43,7 +43,7 @@
       </div>
     </v-col>
     <v-col cols="12" lg="12" class="d-flex align-end justify-end">
-      <h4>V1.1.20210402</h4>
+      <h4>V1.1.20210425</h4>
     </v-col>
 
     <v-dialog
